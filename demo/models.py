b29e05c99694fecf5ab26ef381811ff52162d9f4
mahulst <hulst.michel@gmail.com>
2015-03-07 13:54:15 +0100

    Basic grid displayed where images will be placed

--- demo/models.py
+++ demo/models.py
@@ -1,10 +1,11 @@
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import  RichTextField
 from django.db import models
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailimages.models import Image
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, \
     InlinePanel, PageChooserPanel
+from modelcluster.fields import ParentalKey
 
 class StandardIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -16,7 +17,32 @@ class StandardIndexPage(Page):
         related_name='+'
     )
 
+
+
+class CarouselItem(models.Model):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    caption = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+    class Meta:
+        abstract = True
+
+class StandardIndexPageCarouselItem(Orderable, CarouselItem):
+    page = ParentalKey('demo.StandardIndexPage', related_name='carousel_items')
+
+
 StandardIndexPage.content_panels = [
     FieldPanel('title', classname="full title"),
     FieldPanel('intro', classname="full"),
+    InlinePanel(StandardIndexPage, 'carousel_items', label="Foto's"),
 ]
